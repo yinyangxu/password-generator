26a21da6dfb8bb14f0fada84c3a75ca28258b63d
yinyangxu <yinyangxu@gmail.com>
2013-07-04 10:35:23 +0800

    clean the code

--- password_generator.py
+++ password_generator.py
@@ -7,17 +7,19 @@ import base64
 import string
 
 
-KEY = 'a secret string' # customize this key string
-DIGESTMOD = 'sha1' # support: 'md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512'
+key = 'a secret string' # customize this key string
+digestmod = 'sha1' # support: 'md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512'
 
 
 def generate_password(key, msg, digestmod):
     hmac_obj = hmac.new(key, msg, lambda: hashlib.new(digestmod))
     return base64.b64encode(hmac_obj.digest())
 
+
 def translate_password(password):
     translation_table = string.maketrans('=+/', 'XYZ')
     return password.translate(translation_table)[::-1]
+
 
 def truncate_password(password, length):
     if len(password) < length:
@@ -29,16 +31,20 @@ def truncate_password(password, length):
     return password[start:start+length]
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print 'usage: $ python password_generator.py google'
+def main(argv):
+    if len(argv) != 1:
+        print >> sys.stderr, 'Usage: $ python password_generator.py google'
         sys.exit(2)
-    else:
-        msg = sys.argv[1]
 
-    original_password = generate_password(KEY, msg, DIGESTMOD)
+    msg = argv[0]
+
+    original_password = generate_password(key, msg, digestmod)
     translated_password = translate_password(original_password)
 
     for length in (8, 12, 16, 32):
         password = truncate_password(translated_password, length)
         print '%2d bytes: %s' % (length, password)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
